7dd66fb8e87d46c2bf57c34515185753969d7236
Nalini Prakash <prakash9502853132@gmail.com>
2023-06-19 16:38:26 +0530

    Update googleBot.js

--- googleBot.js
+++ googleBot.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer-extra";
+import puppeteer from "puppeteer";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import express from "express";
 import spawn from "child_process";
